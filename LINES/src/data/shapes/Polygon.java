ef9c9d735505a55d5b77421b08d4db0e554c8ad1
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2014-12-01 01:00:17 -0800

    Updated Polygon contains point method to be more accurate.

--- LINES/src/data/shapes/Polygon.java
+++ LINES/src/data/shapes/Polygon.java
@@ -28,9 +28,27 @@ public class Polygon {
 	
 	public int getNumberOfPoints() { return points; }
 	
+	public boolean contains(Point point) {
+		return contains(point.x, point.y);
+	}
+	
+	// right direction ray odds/evens intersection solution
 	public boolean contains(float X, float Y) {
-		// TODO - make more precise algorithm.
-		return getBounds().contains(X, Y);
+		boolean doesContain = isRightFacingRayFromXYCollidingWithPolygonLinesegmentAB(X, Y, 0, points-1);
+		for(int i = 1; i < getNumberOfPoints(); ++i)
+			if(isRightFacingRayFromXYCollidingWithPolygonLinesegmentAB(X, Y, i, i-1))
+				doesContain = !doesContain;
+		return doesContain;
+	}
+	
+	// if Y is within yRange then we can compute: X <= xBase + (xRange * yPercent)
+	private boolean isRightFacingRayFromXYCollidingWithPolygonLinesegmentAB(float X, float Y, int A, int B) {
+		return isWithinBounds(ypoints[A], Y, ypoints[B])
+		&& (X <= xpoints[A] + (xpoints[B] - xpoints[A]) * (Y - ypoints[A]) / (ypoints[B] - ypoints[A]) );
+	}
+	
+	private boolean isWithinBounds(float A, float Y, float B) {
+		return (Y < A) ^ (Y < B);
 	}
 	
 	public boolean intersects(Rectangle rect) {
@@ -38,11 +56,11 @@ public class Polygon {
 	}
 	
 	private boolean intersectsX(Rectangle rect) {
-		return intersectionRange1D(smallestX(), largestX(), rect.x, rect.x + rect.width);
+		return intersectionRange1D(smallest(xpoints), largest(xpoints), rect.x, rect.x + rect.width);
 	}
 	
 	private boolean intersectsY(Rectangle rect) {
-		return intersectionRange1D(smallestY(), largestY(), rect.y, rect.height);
+		return intersectionRange1D(smallest(ypoints), largest(ypoints), rect.y, rect.y + rect.height);
 	}
 	
 	private boolean intersectionRange1D(int x1, int X1, int x2, int X2) {
@@ -50,39 +68,24 @@ public class Polygon {
 	}
 	
 	public Rectangle getBounds() {
-		int x = smallestX();
-		int y = smallestY();
-		return new Rectangle(x, y, largestX() - x, largestY() - y);
+		int x = smallest(xpoints);
+		int y = smallest(ypoints);
+		return new Rectangle(x, y, largest(xpoints) - x, largest(ypoints) - y);
 	}
 	
-	private int smallestY() {
+	private int smallest(int[] ar) {
 		int result = Integer.MAX_VALUE;
 		for(int i = 0; i < points; ++i)
-			if(ypoints[i] < result)
-				result = ypoints[i];
+			if(ar[i] < result)
+				result = ar[i];
 		return result;
 	}
-	private int smallestX() {
-		int result = Integer.MAX_VALUE;
-		for(int i = 0; i < points; ++i)
-			if(xpoints[i] < result)
-				result = xpoints[i];
-		return result;
-	}
-
-	private int largestY() {
+	
+	private int largest(int[] ar) {
 		int result = Integer.MIN_VALUE;
 		for(int i = 0; i < points; ++i)
-			if(ypoints[i] > result)
-				result = ypoints[i];
+			if(ar[i] > result)
+				result = ar[i];
 		return result;
 	}
-	private int largestX() {
-		int result = Integer.MIN_VALUE;
-		for(int i = 0; i < points; ++i)
-			if(xpoints[i] > result)
-				result = xpoints[i];
-		return result;
-	}
-
 }
