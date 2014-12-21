57f744cfe101f2c67e260162069f50777f76a1ea
dustin <dustin@dustin-pc>
2014-12-21 13:29:06 -0800

    Using generic EditorProgram

--- LINES/src/LineEditor/main/AWTLineEditorProgram.java
+++ LINES/src/LineEditor/main/AWTLineEditorProgram.java
@@ -1,7 +1,7 @@
 package LineEditor.main;
 
+import generic.EditorProgram;
 import AWT.UI.AWTEditorPanel;
-import AWT.UI.AWTLayerManager;
 import AWT.UI.AWTViewportOptionsMenu;
 import AWT.UI.AWTFileMenu;
 import AWT.UI.AWTProgramWindow;
@@ -14,6 +14,7 @@ import LineEditor.AWT.UI.uiTools.AWTLineEditorUserDevice;
 import LineEditor.AWT.rendering.AWTLinesRenderer;
 import LineEditor.data.WorldGeometryData;
 import LineEditor.file.WorldGeometryFiler;
+import UI.LayerManager;
 
 public class AWTLineEditorProgram {
 	
@@ -21,41 +22,39 @@ public class AWTLineEditorProgram {
 		AWTProgramWindow window = new AWTProgramWindow("Lines");
 		
 		WorldGeometryData 			worldData 				= new WorldGeometryData();
-		AWTLinesRenderer			worldRenderer  			= new AWTLinesRenderer(worldData);
 		WorldGeometryFiler 			worldFiler 				= new WorldGeometryFiler();
 		worldFiler.setData(worldData);
-		AWTFileMenu 				fileMenu 				= new AWTFileMenu(worldFiler);
 		AWTLineEditorUserDevice 	lineEditorUserDevice 	= new AWTLineEditorUserDevice(worldData);
 		AWTEditorPanel 				worldEditorPanel 		= new AWTEditorPanel(lineEditorUserDevice);
-		AWTLayerManager 			layerManager 			= new AWTLayerManager();
-		AWTToggleLayersMenu			toggleLayersMenu		= new AWTToggleLayersMenu(layerManager);
-		AWTViewportOptionsMenu 		optionsMenu 			= new AWTViewportOptionsMenu(worldEditorPanel);
+		
+		AWTLinesRenderer			worldRenderer  			= new AWTLinesRenderer(worldData);
 		AWTGridDrawer 				gridDrawer 				= new AWTGridDrawer(worldEditorPanel);
 		AWTScreenShifter			screenShifter 			= new AWTScreenShifter(worldEditorPanel);
+		LayerManager 				layerManager 			= new LayerManager();
+		
 		worldEditorPanel.setLayerManager(layerManager);
 		worldEditorPanel.addMouseMotionListener(screenShifter);
 		worldEditorPanel.addMouseListener(screenShifter);
 		worldEditorPanel.addMouseWheelListener(new AWTZoomWheelListener(worldEditorPanel));
 		window.add(worldEditorPanel);
 
+		AWTToggleLayersMenu	toggleLayersMenu = new AWTToggleLayersMenu(layerManager);
 		toggleLayersMenu.addMenuItemToggleUI("GRID",   gridDrawer);
 		toggleLayersMenu.addMenuItemToggleUI("EDITOR", worldRenderer);
 		
-		layerManager.addLayer(gridDrawer);
-		layerManager.addLayer(worldRenderer);
+		layerManager.addLayers(gridDrawer,
+							   worldRenderer);
 		// Make a menubar which is a set of dropdown menus or roots buttons. Somehow stop the viewport from changing it.
-		layerManager.addLayer(optionsMenu);
-		layerManager.addLayer(toggleLayersMenu);
-		layerManager.addLayer(fileMenu);
+		layerManager.addLayers( new AWTViewportOptionsMenu(worldEditorPanel),
+								toggleLayersMenu,
+								new AWTFileMenu(worldFiler));
 		// end menubar
 		layerManager.addLayer(lineEditorUserDevice);
 		
 		window.revalidate();
 		
-		AWTProgramMain main = new AWTProgramMain();
-		main.setMouse(lineEditorUserDevice);
-		main.setLayerManager(layerManager);
-		Thread mainThread = new Thread(main);
-		mainThread.start();
+		EditorProgram editorProgram = new EditorProgram();
+		editorProgram.setMain(AWTProgramMain.create(layerManager, lineEditorUserDevice));
+		editorProgram.start();
 	}
 }
