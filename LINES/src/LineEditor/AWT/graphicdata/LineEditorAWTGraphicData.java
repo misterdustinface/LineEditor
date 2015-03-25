9322c45e30a5170450e4b38c9bcbe9290ba84d40
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-03-24 19:05:12 -0700

    Updated Table method name

--- LINES/src/LineEditor/AWT/graphicdata/LineEditorAWTGraphicData.java
+++ LINES/src/LineEditor/AWT/graphicdata/LineEditorAWTGraphicData.java
@@ -24,42 +24,42 @@ public class LineEditorAWTGraphicData extends AWTGraphicData implements Singleto
 	}
 	
 	private void loadColors() {
-		colors.put("movePointHighlight", new Color(233,250,51));
-		colors.put("dragLineTracingLine", Color.LIGHT_GRAY);
-		colors.put("dragSelectionBoxBorder", new Color(48,190,56));
-		colors.put("dragSelectionBoxTransparentArea", 
+		colors.insert("movePointHighlight", new Color(233,250,51));
+		colors.insert("dragLineTracingLine", Color.LIGHT_GRAY);
+		colors.insert("dragSelectionBoxBorder", new Color(48,190,56));
+		colors.insert("dragSelectionBoxTransparentArea", 
 				new Color(colors.get("dragSelectionBoxBorder").brighter().getRed(),
 						  colors.get("dragSelectionBoxBorder").brighter().getGreen(),
 						  colors.get("dragSelectionBoxBorder").brighter().getBlue(),
 				64));
-		colors.put("copyToolSelectionBoxBorder", new Color(255,190,64));
-		colors.put("copyToolSelectionBoxInner", 
+		colors.insert("copyToolSelectionBoxBorder", new Color(255,190,64));
+		colors.insert("copyToolSelectionBoxInner", 
 				new Color(colors.get("copyToolSelectionBoxBorder").brighter().getRed(),
 						colors.get("copyToolSelectionBoxBorder").brighter().getGreen(),
 						colors.get("copyToolSelectionBoxBorder").brighter().getBlue(),
 				64));
-		colors.put("cursor", new Color(25,25,25));
-		colors.put("point", Color.BLACK);
-		colors.put("pointCircleHighlight", Color.LIGHT_GRAY);
-		colors.put("selectedPoint", new Color(48,51,255));
-		colors.put("selectedPointCircleHighlight", new Color(151,255,56));
-		colors.put("line", Color.DARK_GRAY);
-		colors.put("lineBoxHighlight", new Color(238,255,56));
-		colors.put("selectedLine", new Color(221,42,63));
-		colors.put("selectedLineBoxHighlight", new Color(255,190,64));
+		colors.insert("cursor", new Color(25,25,25));
+		colors.insert("point", Color.BLACK);
+		colors.insert("pointCircleHighlight", Color.LIGHT_GRAY);
+		colors.insert("selectedPoint", new Color(48,51,255));
+		colors.insert("selectedPointCircleHighlight", new Color(151,255,56));
+		colors.insert("line", Color.DARK_GRAY);
+		colors.insert("lineBoxHighlight", new Color(238,255,56));
+		colors.insert("selectedLine", new Color(221,42,63));
+		colors.insert("selectedLineBoxHighlight", new Color(255,190,64));
 	}
 	
 	private void loadThicknesses() {
-		thicknesses.put("INITIAL_pointSize", graphicData.getThicknessOf("pointSize"));
-		thicknesses.put("INITIAL_pointHighlightCircle", graphicData.getThicknessOf("pointSize") * 3);
-		thicknesses.put("INITIAL_lineHighlightBox", graphicData.getThicknessOf("pointSize") * 2);
-		thicknesses.put("pointHighlightCircle", thicknesses.get("INITIAL_pointHighlightCircle"));
-		thicknesses.put("lineHighlightBox", thicknesses.get("INITIAL_lineHighlightBox"));
+		thicknesses.insert("INITIAL_pointSize", graphicData.getThicknessOf("pointSize"));
+		thicknesses.insert("INITIAL_pointHighlightCircle", graphicData.getThicknessOf("pointSize") * 3);
+		thicknesses.insert("INITIAL_lineHighlightBox", graphicData.getThicknessOf("pointSize") * 2);
+		thicknesses.insert("pointHighlightCircle", thicknesses.get("INITIAL_pointHighlightCircle"));
+		thicknesses.insert("lineHighlightBox", thicknesses.get("INITIAL_lineHighlightBox"));
 	}
 	
 	public void scaleHighlightedWorldGeometry(float scale){
-		thicknesses.put("pointHighlightCircle", (int) (thicknesses.get("pointHighlightCircle") * scale) );
-		thicknesses.put("lineHighlightBox", (int) (thicknesses.get("lineHighlightBox") * scale) );
+		thicknesses.insert("pointHighlightCircle", (int) (thicknesses.get("pointHighlightCircle") * scale) );
+		thicknesses.insert("lineHighlightBox", (int) (thicknesses.get("lineHighlightBox") * scale) );
 	}
 	
 	public void scaleWorldGeometry(float scale) {
@@ -67,8 +67,8 @@ public class LineEditorAWTGraphicData extends AWTGraphicData implements Singleto
 	}
 	
 	public void resetHighlightedWorldGeometryScales(){
-		thicknesses.put("pointHighlightCircle", thicknesses.get("INITIAL_pointHighlightCircle"));
-		thicknesses.put("lineHighlightBox", thicknesses.get("INITIAL_lineHighlightBox"));
+		thicknesses.insert("pointHighlightCircle", thicknesses.get("INITIAL_pointHighlightCircle"));
+		thicknesses.insert("lineHighlightBox", thicknesses.get("INITIAL_lineHighlightBox"));
 	}
 	
 	public void resetWorldGeometryScales(){
