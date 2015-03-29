241e7bad18423104224d96e723c1ca9b21dcb761
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-03-29 11:45:57 -0700

    - LineEditorAWTGraphicData now utilizes the "Named Data" paradigm through the extension of the generic GraphicData class

--- LINES/src/LineEditor/AWT/graphicdata/LineEditorAWTGraphicData.java
+++ LINES/src/LineEditor/AWT/graphicdata/LineEditorAWTGraphicData.java
@@ -1,5 +1,6 @@
 package LineEditor.AWT.graphicdata;
 
+import generic.datastructures.Property;
 import generic.tags.NamedData;
 import generic.tags.Singleton;
 
@@ -24,42 +25,48 @@ public class LineEditorAWTGraphicData extends AWTGraphicData implements Singleto
 	}
 	
 	private void loadColors() {
-		colors.insert("movePointHighlight", new Color(233,250,51));
-		colors.insert("dragLineTracingLine", Color.LIGHT_GRAY);
-		colors.insert("dragSelectionBoxBorder", new Color(48,190,56));
-		colors.insert("dragSelectionBoxTransparentArea", 
-				new Color(colors.get("dragSelectionBoxBorder").brighter().getRed(),
-						  colors.get("dragSelectionBoxBorder").brighter().getGreen(),
-						  colors.get("dragSelectionBoxBorder").brighter().getBlue(),
+		@SuppressWarnings("unchecked")
+		Property<Color> colorProperty = getProperty("color");
+		colorProperty.add("movePointHighlight", new Color(233,250,51));
+		colorProperty.add("dragLineTracingLine", Color.LIGHT_GRAY);
+		colorProperty.add("dragSelectionBoxBorder", new Color(48,190,56));
+		colorProperty.add("dragSelectionBoxTransparentArea", 
+				new Color(getColorOf("dragSelectionBoxBorder").brighter().getRed(),
+						  getColorOf("dragSelectionBoxBorder").brighter().getGreen(),
+						  getColorOf("dragSelectionBoxBorder").brighter().getBlue(),
 				64));
-		colors.insert("copyToolSelectionBoxBorder", new Color(255,190,64));
-		colors.insert("copyToolSelectionBoxInner", 
-				new Color(colors.get("copyToolSelectionBoxBorder").brighter().getRed(),
-						colors.get("copyToolSelectionBoxBorder").brighter().getGreen(),
-						colors.get("copyToolSelectionBoxBorder").brighter().getBlue(),
+		colorProperty.add("copyToolSelectionBoxBorder", new Color(255,190,64));
+		colorProperty.add("copyToolSelectionBoxInner", 
+				new Color(getColorOf("copyToolSelectionBoxBorder").brighter().getRed(),
+						  getColorOf("copyToolSelectionBoxBorder").brighter().getGreen(),
+						  getColorOf("copyToolSelectionBoxBorder").brighter().getBlue(),
 				64));
-		colors.insert("cursor", new Color(25,25,25));
-		colors.insert("point", Color.BLACK);
-		colors.insert("pointCircleHighlight", Color.LIGHT_GRAY);
-		colors.insert("selectedPoint", new Color(48,51,255));
-		colors.insert("selectedPointCircleHighlight", new Color(151,255,56));
-		colors.insert("line", Color.DARK_GRAY);
-		colors.insert("lineBoxHighlight", new Color(238,255,56));
-		colors.insert("selectedLine", new Color(221,42,63));
-		colors.insert("selectedLineBoxHighlight", new Color(255,190,64));
+		colorProperty.add("cursor", new Color(25,25,25));
+		colorProperty.add("point", Color.BLACK);
+		colorProperty.add("pointCircleHighlight", Color.LIGHT_GRAY);
+		colorProperty.add("selectedPoint", new Color(48,51,255));
+		colorProperty.add("selectedPointCircleHighlight", new Color(151,255,56));
+		colorProperty.add("line", Color.DARK_GRAY);
+		colorProperty.add("lineBoxHighlight", new Color(238,255,56));
+		colorProperty.add("selectedLine", new Color(221,42,63));
+		colorProperty.add("selectedLineBoxHighlight", new Color(255,190,64));
 	}
 	
 	private void loadThicknesses() {
-		thicknesses.insert("INITIAL_pointSize", graphicData.getThicknessOf("pointSize"));
-		thicknesses.insert("INITIAL_pointHighlightCircle", graphicData.getThicknessOf("pointSize") * 3);
-		thicknesses.insert("INITIAL_lineHighlightBox", graphicData.getThicknessOf("pointSize") * 2);
-		thicknesses.insert("pointHighlightCircle", thicknesses.get("INITIAL_pointHighlightCircle"));
-		thicknesses.insert("lineHighlightBox", thicknesses.get("INITIAL_lineHighlightBox"));
+		@SuppressWarnings("unchecked")
+		Property<Integer> thicknessProp = getProperty("thickness");
+		thicknessProp.add("INITIAL_pointSize", graphicData.getThicknessOf("pointSize"));
+		thicknessProp.add("INITIAL_pointHighlightCircle", graphicData.getThicknessOf("pointSize") * 3);
+		thicknessProp.add("INITIAL_lineHighlightBox", graphicData.getThicknessOf("pointSize") * 2);
+		thicknessProp.add("pointHighlightCircle", getThicknessOf("INITIAL_pointHighlightCircle"));
+		thicknessProp.add("lineHighlightBox", getThicknessOf("INITIAL_lineHighlightBox"));
 	}
 	
 	public void scaleHighlightedWorldGeometry(float scale){
-		thicknesses.insert("pointHighlightCircle", (int) (thicknesses.get("pointHighlightCircle") * scale) );
-		thicknesses.insert("lineHighlightBox", (int) (thicknesses.get("lineHighlightBox") * scale) );
+		@SuppressWarnings("unchecked")
+		Property<Integer> thicknessProp = getProperty("thickness");
+		thicknessProp.add("pointHighlightCircle", (int) (getThicknessOf("pointHighlightCircle") * scale) );
+		thicknessProp.add("lineHighlightBox", (int) (getThicknessOf("lineHighlightBox") * scale) );
 	}
 	
 	public void scaleWorldGeometry(float scale) {
@@ -67,12 +74,14 @@ public class LineEditorAWTGraphicData extends AWTGraphicData implements Singleto
 	}
 	
 	public void resetHighlightedWorldGeometryScales(){
-		thicknesses.insert("pointHighlightCircle", thicknesses.get("INITIAL_pointHighlightCircle"));
-		thicknesses.insert("lineHighlightBox", thicknesses.get("INITIAL_lineHighlightBox"));
+		@SuppressWarnings("unchecked")
+		Property<Integer> thicknessProp = getProperty("thickness");
+		thicknessProp.add("pointHighlightCircle", getThicknessOf("INITIAL_pointHighlightCircle"));
+		thicknessProp.add("lineHighlightBox", getThicknessOf("INITIAL_lineHighlightBox"));
 	}
 	
 	public void resetWorldGeometryScales(){
-		graphicData.setThicknessOf("pointSize", thicknesses.get("INITIAL_pointSize"));
+		graphicData.setThicknessOf("pointSize", getThicknessOf("INITIAL_pointSize"));
 	}
 	
 }
